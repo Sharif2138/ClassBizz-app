bbc57f3c3380c1e12613c25f27f9db1cadc54d51
Peterobinna <p.nnamchukw@alustudent.com>
2025-11-01 13:31:21 +0200

    feat(student): integrate dashboard route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:classbizz_app/screens/aunthentication/Home_screen.dart';
+import 'package:classbizz_app/screens/student/student_dashboard_screen.dart';
 
 void main() {
   runApp(const ClassBizz());
@@ -8,17 +9,19 @@ void main() {
 class ClassBizz extends StatelessWidget {
   const ClassBizz({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ClassBizz App',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomeScreen(),
+      // 👇👇 Add this block below
+      routes: {
+        '/student/dashboard': (context) => const StudentDashboardScreen(),
+      },
+      home: const StudentDashboardScreen(),
+
     );
   }
 }
-
